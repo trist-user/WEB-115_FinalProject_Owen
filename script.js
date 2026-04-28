5b05cd732b1d8e57846b363b026700aafbaf3ea2
Tristan Owen <towen1@my.waketech.edu>
2026-04-28 12:39:58 -0400

    started on info/event listener for disease info book

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hgshshshs")
+let infoBook = document.getElementById("diseases-info")
+
+let pg1 = document.createElement("p")
+pg1.innerHTML = `<strong>Norovirus</strong>:
+Virus that spreads via close contact, contaminated food, and contaminated surfaces.
+symptoms include vomitting, diarrhea, nausea, and stomach pain `
+infobook.addeventlistener("click", ()=>{
+
+})
